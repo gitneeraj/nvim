7735ca8c2bb58093dea32e7344d6df886c049985
gitneeraj <36226187+gitneeraj@users.noreply.github.com>
2021-12-23 15:59:31 +0530

    add snippts support (#6)
    Co-authored-by: Neeraj A <neeraj.a@LT360.local>

--- lua/plugin-config/nvim-cmp.lua
+++ lua/plugin-config/nvim-cmp.lua
@@ -1,6 +1,8 @@
 local cmp = require'cmp'
 local lspkind = require'lspkind'
 local luasnip = require'luasnip'
+-- friendly snippets
+require("luasnip/loaders/from_vscode").lazy_load()
 
   cmp.setup({
     snippet = {
