cf0a2709af08f10f4b25d8590588bf6aefadac50
Neeraj A <neeraj.a@LT360.local>
2021-12-23 15:42:15 +0530

    fix indentline pipe char

--- lua/plugin-config/nvim-cmp.lua
+++ lua/plugin-config/nvim-cmp.lua
@@ -1,11 +1,12 @@
 local cmp = require'cmp'
 local lspkind = require'lspkind'
+local luasnip = require'luasnip'
 
   cmp.setup({
     snippet = {
       -- REQUIRED - you must specify a snippet engine
       expand = function(args)
-        require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+        luasnip.lsp_expand(args.body) -- For `luasnip` users.
       end,
     },
     mapping = {
