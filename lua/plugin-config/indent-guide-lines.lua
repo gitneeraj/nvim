931f1683a4bcf526556b5fbcbb2b9c6d651ab51b
gitneeraj <36226187+gitneeraj@users.noreply.github.com>
2021-12-23 15:43:29 +0530

    Feature/cleanups (#5)
    * clean up keymap
    
    * fix indentline pipe char
    
    Co-authored-by: Neeraj A <neeraj.a@LT360.local>

--- lua/plugin-config/indent-guide-lines.lua
+++ lua/plugin-config/indent-guide-lines.lua
@@ -2,7 +2,7 @@
 -- 'lukas-reineke/indent-blankline.nvim'
 -- 'Yggdroot/indentLine'
 
-vim.api.nvim_command("let g:indentLine_char = '⎸'")
+vim.api.nvim_command("let g:indentLine_char = '│'")
 vim.api.nvim_command("let g:indentLine_fileTypeExclude = ['text', 'markdown', 'help']")
-vim.api.nvim_command("let g:indentLine_bufNameExclude = ['STARTIFY', 'NVIMTREE']")
+vim.api.nvim_command("let g:indentLine_bufNameExclude = ['STARTIFY']")
 vim.api.nvim_command("let g:indent_blankline_extra_indent_level = -1")
