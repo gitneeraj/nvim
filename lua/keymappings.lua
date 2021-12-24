36d5b633f5dd028eba1ec9f3afeff3e8c1505a0d
Neeraj A <neeraj.a@LT360.local>
2021-12-24 12:50:31 +0530

    minor corrections for my ease use

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -8,10 +8,10 @@ vim.api.nvim_set_keymap('i', 'jj', '<ESC>', opts)
 vim.api.nvim_set_keymap('i', 'kk', '<ESC>', opts)
 
 -- fast scrolling
-vim.api.nvim_set_keymap('n', '<leader>n', '9j', opts)
-vim.api.nvim_set_keymap('n', '<leader>u', '9k', opts)
-vim.api.nvim_set_keymap('v', '<leader>n', '9j', opts)
-vim.api.nvim_set_keymap('v', '<leader>u', '9k', opts)
+vim.api.nvim_set_keymap('n', '<leader>n', '20j', opts)
+vim.api.nvim_set_keymap('n', '<leader>u', '20k', opts)
+vim.api.nvim_set_keymap('v', '<leader>n', '20j', opts)
+vim.api.nvim_set_keymap('v', '<leader>u', '20k', opts)
 
 -- Mapping U to Redo.
 vim.api.nvim_set_keymap('', 'U', '<C-r>', opts)
